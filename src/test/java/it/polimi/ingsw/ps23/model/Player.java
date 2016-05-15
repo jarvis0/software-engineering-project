eb80e297b760fdf0578165f723b5ea89c08e4d19
mirmanz94 <mirco.manzoni@mail.polimi.it>
2016-05-15 21:02:43 +0200

    update player (non è concluso)

--- src/test/java/it/polimi/ingsw/ps23/model/Player.java
+++ src/test/java/it/polimi/ingsw/ps23/model/Player.java
@@ -1,5 +1,71 @@
 package it.polimi.ingsw.ps23.model;
 
+import java.util.ArrayList;
+
+import javax.naming.InsufficientResourcesException;
+
+import it.polimi.ingsw.ps23.model.map.City;
+
 public class Player {
+	
+	private String id;
+	private int victoryPoints;
+	private int nobilityTrackPoints;
+	private int gold;
+	private int assistant;
+	private ArrayList<City> buildedEmporium;
+	//private ArrayList<PermissionHandDeck> permissionHand;
+	//private ArrayList<PoliticHandDeck> politicHand;
+	
+	public Player(String id, int gold,int assistant) {
+		this.id = id;
+		victoryPoints = 0;
+		nobilityTrackPoints = 0;
+		this.gold = gold;
+		this.assistant = assistant;
+		buildedEmporium = new ArrayList<City>();
+		//permissionHand = new ArrayList<PermissionHandDeck>();
+		//politicHand = new ArrayList<PoliticHandDeck>();
+	}
+	
+	public void showPlayerStatus(){
+		
+	}
+	
+	public void pickCard(){
+		
+	}
+	
+	public void updateVictoryPoints(int value){
+		victoryPoints += value;
+	}
+	
+	public void updateNobilityPoints(int value){
+		nobilityTrackPoints += value;
+	}
+	
+	public void updateGolds(int value) throws InsufficientResourcesException{
+		if(gold + value >= 0){
+			gold += value;
+		}
+		else{
+			throw new InsufficientResourcesException();
+		}
+	}
+	
+	public void updateAssistants(int value) throws InsufficientResourcesException{
+		if(assistant + value >= 0){
+			assistant += value;
+		}
+		else{
+			throw new InsufficientResourcesException();
+		}
+	}
+	
+	public int remaingEmporium() {
+		return buildedEmporium.size();// - tutte le città della mappa
+	}
+	
+	
 
 }
