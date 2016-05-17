58f41400e18318643ba5938988119994297ac680
axoaxel <alessandro2.erba@mail.polimi.it>
2016-05-17 15:24:35 +0200

    Add comment

--- src/main/java/it/polimi/ingsw/ps23/model/map/NormalCity.java
+++ src/main/java/it/polimi/ingsw/ps23/model/map/NormalCity.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 import it.polimi.ingsw.ps23.model.Player;
 
 public class NormalCity extends City {
-	RewardToken cityToken;
+ //	RewardToken cityToken;
 	
-	public NormalCity(String name, Color color, RewardToken cityToken){
+	public NormalCity(String name, Color color){ /* RewardToken cityToken)*/
 		this.name = name;
 		this.color = color;
 		emporiumList = new ArrayList<Player>();
-		this.cityToken = cityToken;
+		//this.cityToken = cityToken;
 	}
 }
