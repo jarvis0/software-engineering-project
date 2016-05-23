5184810ef2e8d0294715c96905f044d9b6e7aead
mirmanz94 <mirco.manzoni@mail.polimi.it>
2016-05-23 15:26:14 +0200

    Cities Graph working

--- src/main/java/it/polimi/ingsw/ps23/model/CitiesGraph.java
+++ src/main/java/it/polimi/ingsw/ps23/model/CitiesGraph.java
@@ -3,31 +3,31 @@ package it.polimi.ingsw.ps23.model;
 import java.util.HashMap;
 import java.util.List;
 
-import org.jgrapht.UndirectedGraph;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.SimpleGraph;
 
 import it.polimi.ingsw.ps23.model.map.City;
 
 public class CitiesGraph {	
 	
-	UndirectedGraph<City, DefaultEdge> citiesGraph;
+	DirectedGraph<City, DefaultEdge> citiesGraph;
 	
 	public CitiesGraph(List<String[]> citiesConnections, HashMap<String, City> cities) {
-		citiesGraph = new SimpleGraph<>(DefaultEdge.class);
+		citiesGraph = new DefaultDirectedGraph<City, DefaultEdge>(DefaultEdge.class);
 		for(String[] connections : citiesConnections) {
 			citiesGraph.addVertex(cities.get(connections[0]));
 		}
 		for(String[] connections : citiesConnections) {			
 			for(int i = 1 ; i < connections.length; i++){
-				citiesGraph.addEdge(cities.get(connections[0]), cities.get(connections[i]));
+				citiesGraph.addEdge(cities.get(connections[0]), cities.get(connections[i]));				
 			}
 		}
 	}
 	
 	@Override
 	public String toString() {
-		return "null";
+ 		return "Cities: " + citiesGraph.toString().replace("[(", "\nConnections: [(");
 	}
 	
 
