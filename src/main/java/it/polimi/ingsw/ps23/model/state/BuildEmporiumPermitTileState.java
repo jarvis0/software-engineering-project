3d43493ece7190c1b297f56e661459848118cf60
axoaxel <alessandro2.erba@mail.polimi.it>
2016-06-04 00:34:35 +0200

    Merged on master my last commit. CLI Prototype with all the actions implemented

---
+++ src/main/java/it/polimi/ingsw/ps23/model/state/BuildEmporiumPermitTileState.java
@@ -0,0 +1,54 @@
+package it.polimi.ingsw.ps23.model.state;
+
+import java.util.List;
+import java.util.Map;
+
+import it.polimi.ingsw.ps23.model.Game;
+import it.polimi.ingsw.ps23.model.HandDeck;
+import it.polimi.ingsw.ps23.model.PermissionHandDeck;
+import it.polimi.ingsw.ps23.model.actions.BuildEmporiumPermitTile;
+import it.polimi.ingsw.ps23.model.map.Card;
+import it.polimi.ingsw.ps23.model.map.City;
+import it.polimi.ingsw.ps23.view.visitor.ViewVisitor;
+
+public class BuildEmporiumPermitTileState extends ActionState {
+
+	HandDeck avaibleCards;
+	Map<String, City> citiesMap;
+	List<Card> permissionCards;
+	
+	public BuildEmporiumPermitTileState(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void changeState(Context context, Game game) {
+		context.setState(this);
+		avaibleCards = ((PermissionHandDeck)game.getCurrentPlayer().getPermissionHandDeck()).getAvaiblePermissionCards();
+		citiesMap = game.getGameMap().getCitiesMap();
+		permissionCards = game.getCurrentPlayer().getPermissionHandDeck().getCards();
+	}
+
+	@Override
+	public void acceptView(ViewVisitor view) {
+		view.visit(this);	
+		
+	}
+
+	public String getAvaibleCards() {
+		return avaibleCards.toString();
+	}
+	
+	public String getChosenCard(int index){
+		List<Card> avaibleCardsList = avaibleCards.getCards();
+		String chosenCard = avaibleCardsList.get(index).toString();
+		return chosenCard;
+	}
+
+	public BuildEmporiumPermitTile createAction(String chosenCity, int chosenCard) {
+		return new BuildEmporiumPermitTile(citiesMap.get(chosenCity), chosenCard);
+	}
+	
+
+}
