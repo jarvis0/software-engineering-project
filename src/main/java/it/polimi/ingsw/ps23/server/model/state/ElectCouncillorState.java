d0c6bad33ba87b506c64bb6c3b32b09b1ec2f61d
mirmanz94 <mirco.manzoni@mail.polimi.it>
2016-06-24 11:11:13 +0200

    Solved some issues about actions and rmiconsoleview

--- src/main/java/it/polimi/ingsw/ps23/server/model/state/ElectCouncillorState.java
+++ src/main/java/it/polimi/ingsw/ps23/server/model/state/ElectCouncillorState.java
@@ -1,41 +1,19 @@
 package it.polimi.ingsw.ps23.server.model.state;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import it.polimi.ingsw.ps23.server.model.Game;
 import it.polimi.ingsw.ps23.server.model.actions.Action;
 import it.polimi.ingsw.ps23.server.model.actions.ElectCouncillor;
-import it.polimi.ingsw.ps23.server.model.map.Region;
-import it.polimi.ingsw.ps23.server.model.map.board.FreeCouncillorsSet;
-import it.polimi.ingsw.ps23.server.model.map.regions.Council;
-import it.polimi.ingsw.ps23.server.model.map.regions.GroupRegionalCity;
 import it.polimi.ingsw.ps23.server.view.ViewVisitor;
 
-public class ElectCouncillorState extends ActionState {
+public class ElectCouncillorState extends ElectCouncillorActionState {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4320899244481923736L;
-	private FreeCouncillorsSet freeCouncillors;
-	private Map<String, Council> councilsMap;
 	
 	ElectCouncillorState(String name) {
 		super(name);
-		councilsMap = new HashMap<>();
-	}
-
-	public String getFreeCouncillors() {
-		return freeCouncillors.toString();
-	}
-
-	public String getCouncilsMap() {
-		return councilsMap.toString();
-	}	
-	
-	Council getCouncilMap(String chosenBalcony) {
-		return councilsMap.get(chosenBalcony);
 	}
 
 	public Action createAction(String chosenCouncillor, String chosenBalcony) {
@@ -45,11 +23,7 @@ public class ElectCouncillorState extends ActionState {
 	@Override
 	public void changeState(Context context, Game game) {
 		context.setState(this);
-		freeCouncillors = game.getFreeCouncillors();
-		councilsMap.put("King", game.getKing().getCouncil());
-		for(Region region : game.getGameMap().getGroupRegionalCity()) {
-			councilsMap.put(region.getName(), ((GroupRegionalCity) region).getCouncil());
-		}
+		setParameters(game);
 	}
 
 	@Override
