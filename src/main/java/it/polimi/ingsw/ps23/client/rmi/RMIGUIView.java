0ac77cd28de4e949be95c133a6932b5153d87997
axoaxel <alessandro2.erba@mail.polimi.it>
2016-07-06 13:46:41 +0200

    merge commit

--- src/main/java/it/polimi/ingsw/ps23/client/rmi/RMIGUIView.java
+++ src/main/java/it/polimi/ingsw/ps23/client/rmi/RMIGUIView.java
@@ -434,6 +434,7 @@ public class RMIGUIView extends RMIView implements GUIView {
 
 	@Override
 	public void visit(EndGameState currentState) {
+		swingUI.refreshDynamicContents(currentState);
 		swingUI.appendConsoleText(currentState.getExceptionString());
 		swingUI.appendConsoleText(currentState.getWinner());
 		endGame = true;
